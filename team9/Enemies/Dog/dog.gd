24d195df156fcf13b7a85b5b642dc3ebfecc6c62
Aefka21 <luongxavier77600@gmail.com>
2024-12-08 18:20:22 +0100

    Adjusting enemies

--- team9/Enemies/Dog/dog.gd
+++ team9/Enemies/Dog/dog.gd
@@ -1,8 +1,33 @@
 extends CharacterBody2D
 
 const SPEED = 300.0
-const JUMP_VELOCITY = -400.0
 
+var isAttacking = false
+@export var playerPos: Vector2
 
 func _physics_process(delta: float) -> void:
-	pass
+	var dist = playerPos.distance_to(global_position)
+	if dist < 500 and dist > 10:
+		velocity = position.direction_to(playerPos) * SPEED
+	elif dist > 10:
+		$AnimatedSprite2D.play("Attack")
+		isAttacking = true
+	
+	for i in get_slide_collision_count():
+		var collision = get_slide_collision(i)
+		var player = collision.get_collider()
+		"""if player is Player and not player.isInvincible:
+			player.isInvincible = true
+			player.health -= 10
+			player.showDamage(1, 10)"""
+	
+	if velocity.x < 0:
+		$AnimatedSprite2D.flip_h = false
+	else:
+		$AnimatedSprite2D.flip_h = true
+	move_and_slide()
+
+
+func _on_animated_sprite_2d_animation_finished() -> void:
+	if $AnimatedSprite2D.animation == "Attack":
+		isAttacking = false
--- team9/Enemies/Slime/slime.gd
+++ team9/Enemies/Slime/slime.gd
@@ -1,7 +1,34 @@
 extends CharacterBody2D
 
-
 const SPEED = 300.0
 
+@export var A: Node2D
+@export var B: Node2D
+@export var flip = true
+var toB = true
+
+func move():
+	if toB:
+		velocity.x -= SPEED
+		if abs(position.x - B.position.x) <= 10:
+			velocity.x = 0
+			$AnimatedSprite2D.flip_h = flip
+			toB = false
+	else:
+		velocity.x += SPEED
+		if abs(position.x - A.position.x) <= 10:
+			velocity.x = 0
+			$AnimatedSprite2D.flip_h = not flip
+			toB = true
+
 func _physics_process(delta: float) -> void:
+	move()
 	move_and_slide()
+	
+	for i in get_slide_collision_count():
+		var collision = get_slide_collision(i)
+		var player = collision.get_collider()
+		"""if player is Player and not player.isInvincible:
+			player.isInvincible = true
+			player.health -= 10
+			player.showDamage(1, 10)"""
