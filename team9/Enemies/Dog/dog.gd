2549693ac05125e642f22174d46ba8aa8f88d3ae
Aefka21 <luongxavier77600@gmail.com>
2024-12-09 06:50:42 +0100

    stuff

--- team9/Enemies/Dog/dog.gd
+++ team9/Enemies/Dog/dog.gd
@@ -1,33 +1,43 @@
 extends CharacterBody2D
 
-const SPEED = 300.0
-
+@export var speed = 150
+@export var range = 200
+@export var hp = 100
+var gravity = ProjectSettings.get_setting("physics/2d/default_gravity")
 var isAttacking = false
-@export var playerPos: Vector2
+@onready var player = $"../Player ( Polina)"
 
 func _physics_process(delta: float) -> void:
-	var dist = playerPos.distance_to(global_position)
-	if dist < 500 and dist > 10:
-		velocity = position.direction_to(playerPos) * SPEED
-	elif dist > 10:
+	var dist = player.position.distance_to(position)
+	if dist < range and dist > 50:
+		$AnimatedSprite2D.play("Idle")
+		velocity = position.direction_to(player.position) * speed
+		if velocity.x < 0:
+			$AnimatedSprite2D.flip_h = false
+		else:
+			$AnimatedSprite2D.flip_h = true
+	if dist <= 50:
 		$AnimatedSprite2D.play("Attack")
 		isAttacking = true
 	
-	for i in get_slide_collision_count():
-		var collision = get_slide_collision(i)
-		var player = collision.get_collider()
-		"""if player is Player and not player.isInvincible:
-			player.isInvincible = true
-			player.health -= 10
-			player.showDamage(1, 10)"""
+		for i in get_slide_collision_count():
+			var collision = get_slide_collision(i)
+			var player = collision.get_collider()
+			"""if player is Player:
+				player.isInvincible = true
+				player.health -= 10
+				player.showDamage(1, 10)"""
 	
-	if velocity.x < 0:
-		$AnimatedSprite2D.flip_h = false
-	else:
-		$AnimatedSprite2D.flip_h = true
+	if hp <= 0:
+		$AnimatedSprite2D.play("Death")
+	
+	velocity.y += gravity
 	move_and_slide()
 
 
 func _on_animated_sprite_2d_animation_finished() -> void:
 	if $AnimatedSprite2D.animation == "Attack":
 		isAttacking = false
+		#await get_tree().create_timer(1.0).timeout
+	elif $AnimatedSprite2D.animation == "Death":
+		queue_free()
--- team9/Enemies/Slime/slime.gd
+++ team9/Enemies/Slime/slime.gd
@@ -1,34 +1,60 @@
 extends CharacterBody2D
 
-const SPEED = 300.0
+const SPEED = 100
 
+@onready var player = $"../Player ( Polina)"
+
+var gravity = ProjectSettings.get_setting("physics/2d/default_gravity")
+
+@export var hp = 100
 @export var A: Node2D
 @export var B: Node2D
 @export var flip = true
-var toB = true
+var toB = false
+var isAttacking = false
+
+func _ready() -> void:
+	print("A " + str(A.global_position))
+	print("B " + str(B.global_position))
 
 func move():
 	if toB:
-		velocity.x -= SPEED
-		if abs(position.x - B.position.x) <= 10:
+		velocity.x = SPEED
+		if position.distance_to(B.position) <= 50:
 			velocity.x = 0
 			$AnimatedSprite2D.flip_h = flip
 			toB = false
 	else:
-		velocity.x += SPEED
-		if abs(position.x - A.position.x) <= 10:
+		velocity.x = -SPEED
+		if position.distance_to(A.position) <= 50:
 			velocity.x = 0
 			$AnimatedSprite2D.flip_h = not flip
 			toB = true
 
 func _physics_process(delta: float) -> void:
 	move()
+	velocity.y += gravity
 	move_and_slide()
 	
-	for i in get_slide_collision_count():
-		var collision = get_slide_collision(i)
-		var player = collision.get_collider()
-		"""if player is Player and not player.isInvincible:
-			player.isInvincible = true
-			player.health -= 10
-			player.showDamage(1, 10)"""
+	if position.distance_to(player.position) < 50:
+		$AnimatedSprite2D.play("Attack")
+		isAttacking = true
+	
+	if isAttacking:
+		for i in get_slide_collision_count():
+			var collision = get_slide_collision(i)
+			var player = collision.get_collider()
+			"""if player is Player and not player.isInvincible:
+				player.isInvincible = true
+				player.health -= 10
+				player.showDamage(1, 10)"""
+	
+	if hp <= 0:
+		$AnimatedSprite2D.play("Death")
+
+
+func _on_animated_sprite_2d_animation_finished() -> void:
+	if $AnimatedSprite2D.animation == "Attack":
+		isAttacking = false
+	elif $AnimatedSprite2D.animation == "Death":
+		queue_free()
