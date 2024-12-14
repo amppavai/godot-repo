8e05decc338be8953f869c799fc847157892efe8
Polina99997 <vizerpolina97@gmail.com>
2024-12-14 19:46:50 +0900

    character movement fix
    character movement fix

--- team9/character_body_2d.gd
+++ team9/character_body_2d.gd
@@ -1,32 +1,52 @@
 extends CharacterBody2D
 
+class_name Player
+
+@export var jump_velocity = -300.0
+
 const speed = 100
 var current_dir = "none"
 
-func _physics_process(delta):
-	player_movement(delta)
+var gravity = ProjectSettings.get_setting("physics/2d/default_gravity")
 
-func player_movement(delta):
+func _physics_process(delta):
+	
+	if not is_on_floor():
+		velocity.y += gravity * delta
+	
+	# Handle jump.
+	if Input.is_action_just_pressed("ui_accept") and is_on_floor():
+		velocity.y = jump_velocity
+	
+	player_movement(delta)
+		
+	move_and_slide()
+	
+
+func player_movement(_delta):
 	if Input.is_action_pressed("ui_right"):
 		current_dir = "right"
 		play_anim(1)
 		velocity.x = speed
-		velocity.y = 0
+		#velocity.y = 0
 	elif Input.is_action_pressed("ui_left"):
 		current_dir = "left"
+		play_anim(1)
 		velocity.x = -speed
-		velocity.y = 0
-	elif Input.is_action_pressed("ui_down"):
-		current_dir = "down"
-		velocity.x = 0
-		velocity.y = speed
-	elif Input.is_action_pressed("ui_up"):
-		current_dir = "up"
-		velocity.x = 0
-		velocity.y = -speed
+		#velocity.y = 0
+	#elif Input.is_action_pressed("ui_down"):
+	#	current_dir = "down"
+	#	velocity.x = 0
+	#	velocity.y = speed
+	#elif Input.is_action_pressed("ui_up"):
+	#	current_dir = "up"
+	#	velocity.x = 0
+	#	velocity.y = -speed
 	else:
-		velocity.x = 0
-		velocity.y = 0
+		if is_on_floor():
+			current_dir = "idle"
+			play_anim(1)
+			velocity.x = 0
 
 	move_and_slide()
 	
@@ -38,6 +58,12 @@ func play_anim (movement):
 		anim.flip_h = false
 		if movement  == 1:
 			anim.play("walk")
-		
+	elif dir == "left":
+		anim.flip_h = true
+		if movement  == 1:
+			anim.play("walk")
+	elif dir == "idle":
+		if movement  == 1:
+			anim.play("idle")
 		
 		
