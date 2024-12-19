8fe1c26e996a05e11c112df73b7f203254ca6c0a
amppavai <129379152+amppavai@users.noreply.github.com>
2024-12-19 14:35:28 +0900

    Revert "Merge branch 'main' into Ani_final"
    This reverts commit 40989c69f74e72d0b9c720e5c25f1e665fb2c2f9, reversing
    changes made to 5e415d5e39370b5a46b77e9fb973eb9cff0d0fa0.

--- team9/Enemies/Bat/bat.gd
+++ team9/Enemies/Bat/bat.gd
@@ -3,6 +3,7 @@ extends CharacterBody2D
 
 @export var speed = 300
 @onready var hasTarget = false
+@export var hp = 100
 var target
 var player
 var cd = false
@@ -19,6 +20,9 @@ func _physics_process(delta: float) -> void:
 			target = player.position
 	else:
 		velocity = Vector2(0, 0)
+		
+	if hp <= 0:
+		$AnimatedSprite2D.play("Death")
 			
 	for i in get_slide_collision_count():
 		var collision = get_slide_collision(i)
@@ -32,26 +36,20 @@ func _physics_process(delta: float) -> void:
 	move_and_slide()
 
 func _on_area_2d_body_entered(body: Node2D) -> void:
-	if body is Player:
+	if body.get_collision_layer() == 2:
 		hasTarget = true
 		target = body.position
 		player = body
 
 func _on_area_2d_body_exited(body: Node2D) -> void:
-	if body is Player:
+	if body.get_collision_layer() == 2:
 		hasTarget = false
 
 func _on_animated_sprite_2d_animation_finished() -> void:
 	if $AnimatedSprite2D.animation == "Death":
-		remove_from_group("Enemy")
 		queue_free()
 
 
 func _on_timer_timeout() -> void:
 	$CollisionShape2D.disabled = false
 	cd = false
-
-
-func _on_hit_body_entered(body: Node2D) -> void:
-	if body is Player:
-		$AnimatedSprite2D.play("Death")
